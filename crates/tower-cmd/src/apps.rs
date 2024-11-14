e3722715b582c902c06e51a8af5230d64573ee93
Brad Heller <brad@tower.dev>
2024-11-14 14:55:34 +0100

    feat: Basic `show` command and logs output

--- crates/tower-cmd/src/apps.rs
+++ crates/tower-cmd/src/apps.rs
@@ -14,6 +14,16 @@ pub fn apps_cmd() -> Command {
                 .about("List all of your apps`")
         )
         .subcommand(
+            Command::new("show")
+                .allow_external_subcommands(true)
+                .about("Show the details about an app in Tower")
+        )
+        .subcommand(
+            Command::new("logs")
+                .allow_external_subcommands(true)
+                .about("Get the logs from a previous Tower app run")
+        )
+        .subcommand(
             Command::new("create")
                 .arg(
                     Arg::new("name")
@@ -35,6 +45,85 @@ pub fn apps_cmd() -> Command {
                 .allow_external_subcommands(true)
                 .about("Delete an app in Tower")
         )
+}
+
+pub async fn do_logs_app(_config: Config, client: Client, cmd: Option<(&str, &ArgMatches)>) {
+    let opts = cmd.unwrap_or_else(|| {
+        output::die("App name (e.g. tower apps show <name>#<num>) is required");
+    });
+
+    let (app_name, num) = if opts.0.contains("#") {
+        let parts: Vec<&str> = opts.0.split("#").collect();
+        (parts[0], parts[1])
+    } else {
+        output::die("Run number is required (e.g. tower apps show <name>#<num>)");
+    };
+
+    let spinner = output::spinner("Fetching logs...");
+
+    match client.get_run_logs(&app_name, num).await {
+        Ok(logs) => {
+            spinner.success();
+
+            for li in logs.iter() {
+                output::log_line(&li.timestamp, &li.message, output::LogLineType::Remote);
+            }
+        }, Err(err) => {
+            spinner.failure();
+
+            output::tower_error(err);
+        }
+    }
+}
+
+pub async fn do_show_app(_config: Config, client: Client, cmd: Option<(&str, &ArgMatches)>) {
+    let opts = cmd.unwrap_or_else(|| {
+        output::die("App name (e.g. tower apps show <name>) is required");
+    });
+
+    match client.get_app(&opts.0).await {
+        Ok((app, runs)) => {
+            let line = format!("{} {}\n", "Name:".bold().green(), app.name);
+            output::write(&line);
+
+            let line = format!("{}\n", "Description:".bold().green());
+            output::write(&line);
+
+            let line = output::paragraph(&app.short_description);
+            output::write(&line);
+
+            output::newline();
+            output::newline();
+
+            let line = format!("{}\n", "Recent runs:".bold().green());
+            output::write(&line);
+    
+            let headers = vec![
+                "#".yellow().to_string(),
+                "Status".yellow().to_string(),
+                "Start Time".yellow().to_string(),
+                "Elapsed Time".yellow().to_string(),
+            ];
+
+            let rows = runs.iter().map(|run| {
+                let status = run.status.clone();
+                let start_time = run.started_at.unwrap().to_string();
+                let elapsed_time = "".to_string();
+
+                vec![
+                    run.number.to_string(),
+                    status.to_string(),
+                    start_time,
+                    elapsed_time,
+                ]
+            }).collect();
+
+            output::table(headers, rows);
+        },
+        Err(err) => {
+            output::tower_error(err);
+        }
+    }
 }
 
 pub async fn do_list_apps(_config: Config, client: Client) {
