4f273bfa1920052c5ea6495bd4b2c7a4b046ac7c
Brad Heller <brad@tower.dev>
2025-02-26 19:36:47 +0100

    chore: Really basic (but working) Python SDK

--- src/tower/client.py
+++ src/tower/client.py
@@ -1,25 +1,37 @@
+import os
+from typing import Any, Dict, Optional
+
 from .tower_api_client import AuthenticatedClient
-from .tower_api_client.api.default import run_app
-from .tower_api_client.models import RunAppParams
+from .tower_api_client.api.default import run_app as run_app_api
+from .tower_api_client.models import (
+    RunAppParamsParameters,
+    RunAppParams,
+    RunAppResponse,
+)
+
+def _env_client() -> AuthenticatedClient:
+    api_key = os.getenv("TOWER_API_KEY")
+    return AuthenticatedClient(
+        verify_ssl=False,
+        base_url='http://localhost:8081/v1',
+        token=api_key,
+        auth_header_name='X-API-Key',
+        prefix='',
+    )
 
 
-class Client:
-    def __init__(self, token: str):
-        self.client = AuthenticatedClient(
-            base_url="http://localhost:8081",
-            token=token,
-        )
+def run_app(name: str):
+    client = _env_client()
+    params = RunAppParamsParameters()
 
-    def run_app(self, name: str):
-        with self.client as client:
-            input_body = run_app.RunAppParams(
-                environment="default",
-                parameters={},
-            )
+    input_body = RunAppParams(
+        environment="default",
+        parameters=params,
+    )
 
-            output: RunAppOutput = run_app.sync(
-                name,
-                client=client,
-                body=input_body
-            )
-            print(body)
+    output: RunAppResponse = run_app_api.sync(
+        name=name,
+        client=client,
+        json_body=input_body
+    )
+    print(output)
