baa7b7c1230c6244c68f218b15455f65cc414d90
Rohit Sankaran <rohit.sankaran@gmail.com>
2025-02-26 09:39:08 +0800

    fix: Update version function signature

--- crates/tower-cmd/src/version.rs
+++ crates/tower-cmd/src/version.rs
@@ -1,14 +1,12 @@
 use clap::Command;
 use crate::output;
-use tower_api::apis::configuration::Configuration;
-use config::Config;
 
 pub fn version_cmd() -> Command {
     Command::new("version")
         .about("Print the current version of Tower")
 }
 
-pub async fn do_version(_config: Config, _configuration: &Configuration) {
+pub async fn do_version() {
     let line = format!("v{}\n", tower_version::current_version());
     output::write(&line);
 }
