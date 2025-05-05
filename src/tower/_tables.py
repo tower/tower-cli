ee58e60ede5a2d1eb531e47380773b27b13e1703
Brad Heller <brad@tower.dev>
2025-05-05 17:17:48 +0200

    chore: Add new methods to the Table function

--- src/tower/_tables.py
+++ src/tower/_tables.py
@@ -1,17 +1,30 @@
-from typing import Optional
+from typing import Optional, Generic, TypeVar, Union, List
+from dataclasses import dataclass
+
+TTable = TypeVar("TTable", bound="Table")
 
 import polars as pl
 import pyarrow as pa
+import pyarrow.compute as pc
 
 from pyiceberg.catalog import load_catalog
 from pyiceberg.table import Table as IcebergTable
 
 from ._context import TowerContext
-from .utils.pyarrow import convert_pyarrow_schema
+from .utils.pyarrow import (
+    convert_pyarrow_schema,
+    convert_pyarrow_expressions,
+)
 from .utils.tables import (
     make_table_name,
     namespace_or_default,
 )
+
+@dataclass
+class RowsAffectedInformation:
+    inserts: int
+    updates: int
+
 
 class Table:
     """
@@ -20,8 +33,10 @@ class Table:
     """
 
     def __init__(self, context: TowerContext, table: IcebergTable):
+        self._stats = RowsAffectedInformation(0, 0)
         self._context = context
         self._table = table
+
 
     def read(self) -> pl.DataFrame:
         """
@@ -31,14 +46,104 @@ class Table:
         # the result as a DataFrame.
         return pl.scan_iceberg(self._table).collect()
 
-    def insert(self, data: pa.Table):
+
+    def to_polars(self) -> pl.LazyFrame:
+        """
+        Converts the table to a Polars LazyFrame. This is useful when you
+        understand Polars and you want to do something more complicated.
+        """
+        return pl.scan_iceberg(self._table).collect()
+
+
+    def rows_affected(self) -> RowsAffectedInformation:
+        """
+        Returns the stats for the table. This includes the number of inserts,
+        updates, and deletes.
+        """
+        return self._stats
+
+
+    def insert(self, data: pa.Table) -> TTable:
         """
         Inserts data into the Iceberg table. The data is expressed as a PyArrow table.
 
         Args:
             data (pa.Table): The data to insert into the table.
+
+        Returns:
+            TTable: The table with the inserted rows.
         """
         self._table.append(data)
+        self._stats.inserts += data.num_rows()
+        return self
+
+
+    def upsert(self, data: pa.Table, join_cols: Optional[list[str]] = None) -> TTable:
+        """
+        Upserts data into the Iceberg table. The data is expressed as a PyArrow table.
+
+        Args:
+            data (pa.Table): The data to upsert into the table.
+            join_cols (Optional[list[str]]): The columns that form the key to match rows on
+
+        Returns:
+            TTable: The table with the upserted rows.
+        """
+        res = self._table.upsert(
+            data,
+            join_cols=join_cols,
+
+            # All upserts will always be case sensitive. Perhaps we'll add this
+            # as a parameter in the future?
+            case_sensitive=True,
+            
+            # These are the defaults, but we're including them to be complete.
+            when_matched_update_all=True,
+            when_not_matched_insert_all=True,
+        )
+
+        # Update the stats with the results of the relevant upsert.
+        self._stats.updates += res.rows_updated
+        self._stats.inserts += res.rows_inserted
+
+        return self
+
+
+    def delete(self, filters: Union[str, List[pc.Expression]]) -> TTable:
+        """
+        Deletes data from the Iceberg table. The filters are expressed as a
+        PyArrow expression. The filters are applied to the table and the
+        matching rows are deleted.
+
+        Args:
+            filters (Union[str, List[pc.Expression]]): The filters to apply to the table.
+                This can be a string or a list of PyArrow expressions.
+
+        Returns:
+            TTable: The table with the deleted rows.
+        """
+        if isinstance(filters, list):
+            # We need to covnert the pc.Expression into PyIceberg
+            next_filters = convert_pyarrow_expression(filters)
+            filters = next_filters
+
+        self._table.delete(
+            delete_filters=filters,
+
+            # We want this to always be the case. Not sure why you wouldn't?
+            case_sensitive=True,
+        )
+
+        # NOTE: There is, unfortunately, no way to get the number of rows
+        # deleted besides comparing the two snapshots that were created.
+
+        return self
+
+    def schema(self) -> pa.Schema:
+        # We take an Iceberg Schema and we need to convert it into a PyArrow Schema
+        iceberg_schema = self._table.schema()
+        return convert_iceberg_schema(iceberg_schema)
+
 
 class TableReference:
     def __init__(self, ctx: TowerContext, catalog_name: str, name: str, namespace: Optional[str] = None):
@@ -47,11 +152,13 @@ class TableReference:
         self._name = name
         self._namespace = namespace
 
+
     def load(self) -> Table:
         namespace = namespace_or_default(self._namespace)
         table_name = make_table_name(self._name, namespace)
         table = self._catalog.load_table(table_name)
         return Table(self._context, table)
+
 
     def create(self, schema: pa.Schema) -> Table:
         namespace = namespace_or_default(self._namespace)
@@ -71,6 +178,7 @@ class TableReference:
 
         return Table(self._context, table)
 
+
     def create_if_not_exists(self, schema: pa.Schema) -> Table:
         namespace = namespace_or_default(self._namespace)
         table_name = make_table_name(self._name, namespace)
@@ -88,6 +196,7 @@ class TableReference:
         )
 
         return Table(self._context, table)
+
 
 
 def tables(
--- src/tower/utils/pyarrow.py
+++ src/tower/utils/pyarrow.py
@@ -1,6 +1,18 @@
+from typing import Optional, List
+
 import pyarrow as pa
+import pyarrow.compute as pc
+
 import pyiceberg.types as types
 from pyiceberg.schema import Schema as IcebergSchema
+from pyiceberg.expressions import (
+    BooleanExpression,
+    And, Or, Not,
+    EqualTo, NotEqualTo,
+    GreaterThan, GreaterThanOrEqual,
+    LessThan, LessThanOrEqual,
+    Literal, Reference
+)
 
 def arrow_to_iceberg_type(arrow_type):
     """
@@ -53,6 +65,52 @@ def arrow_to_iceberg_type(arrow_type):
     else:
         raise ValueError(f"Unsupported Arrow type: {arrow_type}")
 
+
+def iceberg_to_arrow_type(iceberg_type):
+    """
+    Convert a PyIceberg type to a PyArrow type.
+    """
+    if isinstance(iceberg_type, types.BooleanType):
+        return pa.bool_()
+    elif isinstance(iceberg_type, types.IntegerType):
+        return pa.int32()
+    elif isinstance(iceberg_type, types.LongType):
+        return pa.int64()
+    elif isinstance(iceberg_type, types.FloatType):
+        return pa.float32()
+    elif isinstance(iceberg_type, types.DoubleType):
+        return pa.float64()
+    elif isinstance(iceberg_type, types.StringType):
+        return pa.string()
+    elif isinstance(iceberg_type, types.BinaryType):
+        return pa.binary()
+    elif isinstance(iceberg_type, types.DateType):
+        return pa.date32()
+    elif isinstance(iceberg_type, types.TimestampType):
+        # Using microsecond precision as default
+        return pa.timestamp('us')
+    elif isinstance(iceberg_type, types.TimeType):
+        # Using microsecond precision as default
+        return pa.time64('us')
+    elif isinstance(iceberg_type, types.DecimalType):
+        return pa.decimal128(iceberg_type.precision, iceberg_type.scale)
+    elif isinstance(iceberg_type, types.ListType):
+        element_type = iceberg_to_arrow_type(iceberg_type.element_type)
+        return pa.list_(element_type)
+    elif isinstance(iceberg_type, types.StructType):
+        arrow_fields = []
+        for field in iceberg_type.fields:
+            arrow_type = iceberg_to_arrow_type(field.field_type)
+            arrow_fields.append(pa.field(field.name, arrow_type, nullable=not field.required))
+        return pa.struct(arrow_fields)
+    elif isinstance(iceberg_type, types.MapType):
+        key_type = iceberg_to_arrow_type(iceberg_type.key_type)
+        value_type = iceberg_to_arrow_type(iceberg_type.value_type)
+        return pa.map_(key_type, value_type)
+    else:
+        raise ValueError(f"Unsupported Iceberg type: {iceberg_type}")
+
+
 def convert_pyarrow_field(num, field) -> types.NestedField:
     name = field.name
     field_type = arrow_to_iceberg_type(field.type)
@@ -65,7 +123,55 @@ def convert_pyarrow_field(num, field) -> types.NestedField:
         required=not field.nullable
     )
 
+
+def convert_iceberg_field(field) -> pa.Field:
+    """Convert a PyIceberg NestedField to a PyArrow Field."""
+    name = field.name
+    arrow_type = iceberg_to_arrow_type(field.field_type)
+    
+    return pa.field(name, arrow_type, nullable=not field.required)
+
+
 def convert_pyarrow_schema(arrow_schema: pa.Schema) -> IcebergSchema:
     """Convert a PyArrow schema to a PyIceberg schema."""
     fields = [convert_pyarrow_field(i, field) for i, field in enumerate(arrow_schema)]
     return IcebergSchema(*fields)
+
+
+def convert_iceberg_schema(iceberg_schema: IcebergSchema) -> pa.Schema:
+    """Convert a PyIceberg schema to a PyArrow schema."""
+    arrow_fields = [convert_iceberg_field(field) for field in iceberg_schema.fields]
+    return pa.schema(arrow_fields)
+
+
+def convert_pyarrow_expression(expr: pc.Expression) -> Optional[BooleanExpression]:
+    if expr is None:
+        return None
+
+    if expr.op == "and":
+        return And(convert_expression(expr.args[0]), convert_expression(expr.args[1]))
+    elif expr.op == "or":
+        return Or(convert_expression(expr.args[0]), convert_expression(expr.args[1]))
+    elif expr.op == "not":
+        return Not(convert_expression(expr.args[0]))
+    elif expr.op in {"==", "equal"}:
+        return EqualTo(Reference(expr.args[0].name), expr.args[1].as_py())
+    elif expr.op in {"!=", "not_equal"}:
+        return NotEqualTo(Reference(expr.args[0].name), expr.args[1].as_py())
+    elif expr.op in {">", "greater"}:
+        return GreaterThan(Reference(expr.args[0].name), expr.args[1].as_py())
+    elif expr.op == ">=":
+        return GreaterThanOrEqual(Reference(expr.args[0].name), expr.args[1].as_py())
+    elif expr.op == "<":
+        return LessThan(Reference(expr.args[0].name), expr.args[1].as_py())
+    elif expr.op == "<=":
+        return LessThanOrEqual(Reference(expr.args[0].name), expr.args[1].as_py())
+    else:
+        raise ValueError(f"Unsupported operation: {expr.op}")
+
+
+def convert_pyarrow_expressions(exprs: List[pc.Expression]) -> List[BooleanExpression]:
+    """
+    Convert a list of PyArrow expressions to PyIceberg expressions.
+    """
+    return [convert_pyarrow_expression(expr) for expr in exprs]
