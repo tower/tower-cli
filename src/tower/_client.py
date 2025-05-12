07111d72f12cf492a9400c9ec128831e4fba3f98
Brad Heller <brad@tower.dev>
2025-05-12 14:40:45 +0100

    chore: Feedback from @datancoffee

--- src/tower/_client.py
+++ src/tower/_client.py
@@ -3,6 +3,15 @@ import time
 from typing import List, Dict, Optional
 
 from ._context import TowerContext
+from ._errors import (
+    NotFoundException,
+    UnauthorizedException,
+    UnknownException,
+    UnhandledRunStateException,
+    RunFailedError,
+    TimeoutException,
+)
+
 from .tower_api_client import AuthenticatedClient
 from .tower_api_client.api.default import describe_run as describe_run_api
 from .tower_api_client.api.default import run_app as run_app_api
@@ -101,20 +110,63 @@ def run_app(
             return output.run
 
 
-def wait_for_run(run: Run) -> None:
+def _is_failed_run(run: Run) -> bool:
+    """
+    Check if the given run has failed.
+
+    Args:
+        run (Run): The Run object containing the status to check.
+
+    Returns:
+        bool: True if the run has failed, False otherwise.
+    """
+    return run.status in ["crashed", "cancelled", "errored"] 
+
+
+def _is_successful_run(run: Run) -> bool:
+    """
+    Check if a given run was successful.
+
+    Args:
+        run (Run): The Run object containing the status to check.
+
+    Returns:
+        bool: True if the run was successful, False otherwise.
+    """
+    return run.status in ["exited"]
+
+
+def _is_run_awaiting_completion(run: Run) -> bool:
+    """
+    Check if a given run is either running or expected to run in the near future.
+
+    Args:
+       run (Run): The Run object containing the status to check.
+
+    Returns:
+        bool: True if the run is awaiting run or currently running, False otherwise.
+    """
+    return run.status in ["pending", "scheduled", "running"]
+
+
+def wait_for_run(
+    run: Run,
+    timeout: Optional[float] = 86_400.0, # one day
+    raise_on_failure: bool = False,
+) -> Run:
     """
     Wait for a Tower app run to reach a terminal state by polling the Tower API.
 
     This function continuously polls the Tower API every 2 seconds (defined by WAIT_TIMEOUT)
     to check the status of the specified run. The function returns when the run reaches
-    any of the following terminal states:
-    - exited: The run completed successfully
-    - failed: The run failed during execution
-    - canceled: The run was manually canceled
-    - errored: The run encountered an error
+    any of the defined terminal states.
 
     Args:
         run (Run): The Run object containing the app_slug and number of the run to monitor.
+        timeout (Optional[float]): An optional timeout for this wait. Defaults
+            to one day (86,000 seconds).
+        raise_on_failure (bool): Whether to raise an exception when a failure
+            occurs. Defaults to False.
 
     Returns:
         None: This function does not return any value.
@@ -126,6 +178,9 @@ def wait_for_run(run: Run) -> None:
     ctx = TowerContext.build()
     client = _env_client(ctx)
 
+    # We use this to track the timeout, if one is defined.
+    start_time = time.time()
+
     while True:
         output: Optional[Union[DescribeRunResponse, ErrorModel]] = describe_run_api.sync(
             slug=run.app_slug,
@@ -134,26 +189,51 @@ def wait_for_run(run: Run) -> None:
         )
 
         if output is None:
-            raise RuntimeError("Error fetching run")
+            raise UnknownException("Error fetching run")
         else:
             if isinstance(output, ErrorModel):
-                raise RuntimeError(f"Error fetching run: {output.title}")
+                # If it was a 404 error, that means that we couldn't find this
+                # app for some reason. This is really only relevant on the
+                # first time that we check--if we could find the run, but then
+                # suddenly couldn't that's a really big problem I'd say.
+                if output.status == 404:
+                    raise NotFoundException(output.detail)
+                elif output.status == 401:
+                    # NOTE: Most of the time, this shouldn't happen?
+                    raise UnauthorizedException(output.detail)
+                else:
+                    raise UnknownException(output.detail)
             else:
                 desc = output.run
 
-                if desc.status == "exited":
-                    return
-                elif desc.status == "failed":
-                    return
-                elif desc.status == "canceled":
-                    return
-                elif desc.status == "errored":
-                    return
-                else:
+                if _is_successful_run(desc):
+                    return True
+                elif _is_failed_run(desc):
+                    if raise_on_failure:
+                        raise RunFailedError(desc.app_slug, desc.number)
+                    else:
+                        return False
+
+                elif _is_run_awaiting_completion(desc):
                     time.sleep(WAIT_TIMEOUT)
+                else:
+                    raise UnhandledRunStateException(desc.status)
+
+                # Before we head back to the top of the loop, let's see if we
+                # should timeout
+                if timeout is not None:
+                    # The user defined a timeout, so let's actually see if we
+                    # reached it.
+                    t = time.time() - start_time
+                    if t > timeout:
+                        raise TimeoutException(t)
 
 
-def wait_for_runs(runs: List[Run]) -> None:
+def wait_for_runs(
+    runs: List[Run],
+    timeout: Optional[float] = 86_400.0, # one day
+    raise_on_failure: bool = False,
+) -> tuple[List[Run], List[Run]]:
     """
     `wait_for_runs` waits for a list of runs to reach a terminal state by
     polling the Tower API every 2 seconds for the latest status. If any of the
@@ -162,6 +242,9 @@ def wait_for_runs(runs: List[Run]) -> None:
 
     Args:
         runs (List[Run]): A list of Run objects to monitor.
+        timeout (Optional[float]): Timeout to wait.
+        raise_on_failure (bool): If true, raises an exception when
+            any one of the awaited runs fails. Defaults to False.
 
     Returns:
         None: This function does not return any value.
@@ -171,4 +254,8 @@ def wait_for_runs(runs: List[Run]) -> None:
             of the runs fail.
     """
     for run in runs:
-        wait_for_run(run)
+        wait_for_run(
+            run,
+            timeout=timeout,
+            raise_on_failure=raise_on_failure,
+        )
